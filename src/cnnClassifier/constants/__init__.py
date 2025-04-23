6efad8eb50ba538736dcf5c1b911ea8328dad3bd
HemaPrabha1973 <hemanand96@gmail.com>
2025-04-23 16:23:41 +0530

    changes updated

---
+++ src/cnnClassifier/constants/__init__.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("C:/Users/Admin/Documents/Cancer_Project/End-to-End-Cancer-Classification-with-MLFlow/config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
